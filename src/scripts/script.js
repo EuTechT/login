7222cf0d73ed75d32ff7455bf01d03afb94e4a46
André Filipe <eutech.backup@gmail.com>
2021-06-14 18:52:22 -0300

    Form validation done

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,44 @@
 const form = window.document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
-    console.log(event);
+    // console.log(event);
     event.preventDefault();
 });
+
+const fields = window.document.querySelectorAll("[required]");
+// console.log(fields);
+
+function customValidation(event){
+    // console.log(event);
+
+    const field = event.target;
+    // console.log(field);
+
+    // console.log(field.validity);
+    function verifyErrors(){
+        let foundError = false;
+
+        for(let error in field.validity){
+            // console.log(error);
+
+            if(error != "customError" && field.validity[error]){
+                foundError = error;
+            }
+        }
+
+        return foundError;
+    }
+
+    const error = verifyErrors();
+    console.log("Error Exists: ", error);
+
+    if(error){
+        field.setCustomValidity("Esse campo é obrigatório!");
+    } else {
+        field.setCustomValidity("");
+    }
+}
+
+for(field of fields){
+    field.addEventListener("invalid", customValidation);
+}
